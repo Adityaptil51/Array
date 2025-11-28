a68aa5b17d796ccf262646697f3c1afd5382b02d
Adityaptil51 <adityaapatil51@gmail.com>
2025-11-28 18:44:03 +0530

    saved new

--- Scores.py
+++ Scores.py
@@ -13,6 +13,10 @@ else:
 
 total = sum(scores)
 average = total / len(scores)
+maximum = max(scores)
+minimum = min(scores)
 
 print(f"Sum of scores = {total}")
 print(f"Average score = {average:.2f}")
+print(f"Maximum score = {maximum}")
+print(f"Minimum score = {minimum}")
